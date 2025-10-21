8f51ff3a2e324eacc2b2c306e1a187ec61c9913f
ElPunch <alfonsorobledo3548@gmail.com>
2025-10-20 19:31:04 -0600

    the creation of the QueueManagement

---
+++ src/ManagementDataStrucutre/QueueManagement.java
@@ -0,0 +1,31 @@
+package ManagementDataStrucutre;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class QueueManagement {
+    public static void main(String[] args) {
+        Queue<String> queue = new LinkedList<>();
+        Scanner input = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Introduce el numero que desea agregar o oprime c para salir: ");
+            String valor = input.next();
+
+            if (valor.equalsIgnoreCase("c")){
+                break;
+            }
+
+            queue.add(input.next());
+        }
+
+        System.out.println("Lista actual: "+ queue);
+
+        System.out.println("Primero en la lista: "+ queue.peek());
+
+        System.out.println("Primer elemento en eliminar: "+ queue.poll());
+
+        System.out.println("Lista Actualizada: "+ queue);
+    }
+}
