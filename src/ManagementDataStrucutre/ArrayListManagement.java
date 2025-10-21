dd0bb51b2d596274c16098a1d61a0c4049913b1c
ElPunch <alfonsorobledo3548@gmail.com>
2025-10-20 19:16:34 -0600

    the creation of the code arraylist

--- src/ManagementDataStrucutre/ArrayListManagement.java
+++ src/ManagementDataStrucutre/ArrayListManagement.java
@@ -1,4 +1,42 @@
 package ManagementDataStrucutre;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class ArrayListManagement {
+
+    public static void main(String[] args) {
+        List<String> lista = new ArrayList<String>();
+        Scanner input = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Introduce el numero que desea agregar o oprime c para salir: ");
+            String valor = input.next();
+
+            if (valor.equalsIgnoreCase("c")){
+                break;
+            }
+
+            lista.add(input.next());
+        }
+
+        System.out.println("Lista actual: "+ lista);
+
+        System.out.println("Posicion a mover el numero: ");
+        int posicion = input.nextInt();
+
+        if (lista.size() > 1 && posicion < lista.size()) {
+            String mover = lista.remove(0);
+            lista.add(posicion, mover);
+        } else {
+            System.out.println("No existe el numero que desea agregar");
+        }
+
+        System.out.println("lista modificada: " + lista);
+
+        input.close();
+
+
+    }
 }
